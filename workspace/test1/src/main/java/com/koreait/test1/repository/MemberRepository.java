571b6874259a45a439b23b8ad046084289701323
seulki412 <116873132+seulki412@users.noreply.github.com>
2022-11-26 23:54:16 +0900

    spring-study

---
+++ workspace/test1/src/main/java/com/koreait/test1/repository/MemberRepository.java
@@ -0,0 +1,17 @@
+package com.koreait.test1.repository;
+
+import java.util.List;
+
+
+
+import com.koreait.test1.member.Member;
+
+
+public interface MemberRepository {
+
+	// 회원 저장
+	Member save(Member member);
+	
+	// 회원 전체 찾기
+	List<Member> findAll();
+}
